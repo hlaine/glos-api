d14b0aaecf09e18bb0af5b70fc9d47706ea5e0ac
Henry Laine <henry@deftly.se>
2021-04-06 01:36:58 +0200

    Allow calls from react port 3000

--- src/main/java/se/deftly/glosapi/api/QuizController.java
+++ src/main/java/se/deftly/glosapi/api/QuizController.java
@@ -2,12 +2,14 @@ package se.deftly.glosapi.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import se.deftly.glosapi.domain.Word;
 import se.deftly.glosapi.service.WordService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class QuizController {
 
